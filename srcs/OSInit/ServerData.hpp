1292f30fc0ed56897ed29b3d55265100751ccf07
JIA0010 <HIROsroutine@gmail.com>
2025-01-27 10:26:06 +0900

    直接iserver持ってきた

--- srcs/OSInit/ServerData.hpp
+++ srcs/OSInit/ServerData.hpp
@@ -12,7 +12,20 @@
 #include <string>
 #include <vector>
 
-#include "IServerFunctions.hpp"
+class IServerFunctions {
+ public:
+  virtual ~IServerFunctions() {}
+  virtual void server_bind() = 0;
+  virtual void server_listen() = 0;
+  virtual void server_accept() = 0;
+  virtual void set_address_data() = 0;
+  virtual void set_server_fd() = 0;
+  virtual int get_server_fd() const = 0;
+  virtual int get_new_socket() const = 0;
+  virtual struct sockaddr_in get_address() const = 0;
+  virtual int get_addrlen() const = 0;
+  virtual void set_new_socket(int new_socket) = 0;
+};
 
 class ServerData : public IServerFunctions {
  private:
