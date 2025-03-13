c4b72503df929d8c2127284640d3ec7fc5c1e0e7
smizuoch <smizuoch@student.42tokyo.jp>
2025-03-13 13:58:04 +0900

    fix fmt test

--- test/srcs/TOMLParserTest.cpp
+++ test/srcs/TOMLParserTest.cpp
@@ -9,13 +9,9 @@
 
 class TOMLParserTest : public ::testing::Test {
  protected:
-  virtual void SetUp() {
-    parser = new TOMLParser();
-  }
+  virtual void SetUp() { parser = new TOMLParser(); }
 
-  virtual void TearDown() {
-    delete parser;
-  }
+  virtual void TearDown() { delete parser; }
 
   TOMLParser* parser;
 
@@ -36,14 +32,15 @@ class TOMLParserTest : public ::testing::Test {
 TEST_F(TOMLParserTest, BasicParsing) {
   std::string content = "[http]\nserver_name = webserv\nport = 8080\n";
   Directive* directive = parser->parseFromString(content);
-  
+
   ASSERT_NE(directive, (Directive*)NULL);
   EXPECT_EQ(directive->children().size(), 1);
   EXPECT_EQ(directive->children()[0].name(), "http");
   EXPECT_EQ(directive->children()[0].keyValues().size(), 2);
-  EXPECT_EQ(directive->children()[0].keyValues().at("server_name")[0], "webserv");
+  EXPECT_EQ(directive->children()[0].keyValues().at("server_name")[0],
+            "webserv");
   EXPECT_EQ(directive->children()[0].keyValues().at("port")[0], "8080");
-  
+
   delete directive;
 }
 
@@ -51,18 +48,19 @@ TEST_F(TOMLParserTest, BasicParsing) {
 TEST_F(TOMLParserTest, ParseFromFile) {
   const std::string filename = "temp_toml_test.toml";
   std::string content = "[http]\nserver_name = webserv\nport = 8080\n";
-  
+
   createTempFile(filename, content);
-  
+
   Directive* directive = parser->parseFromFile(filename);
-  
+
   ASSERT_NE(directive, (Directive*)NULL);
   EXPECT_EQ(directive->children().size(), 1);
   EXPECT_EQ(directive->children()[0].name(), "http");
   EXPECT_EQ(directive->children()[0].keyValues().size(), 2);
-  EXPECT_EQ(directive->children()[0].keyValues().at("server_name")[0], "webserv");
+  EXPECT_EQ(directive->children()[0].keyValues().at("server_name")[0],
+            "webserv");
   EXPECT_EQ(directive->children()[0].keyValues().at("port")[0], "8080");
-  
+
   delete directive;
   removeTempFile(filename);
 }
@@ -77,87 +75,107 @@ TEST_F(TOMLParserTest, NonExistentFile) {
 TEST_F(TOMLParserTest, EmptyContent) {
   std::string content = "";
   Directive* directive = parser->parseFromString(content);
-  
+
   ASSERT_NE(directive, (Directive*)NULL);
   EXPECT_EQ(directive->children().size(), 0);
-  
+
   delete directive;
 }
 
 // コメント行のテスト
 TEST_F(TOMLParserTest, CommentLines) {
-  std::string content = "[http]\n# This is a comment\nserver_name = webserv\n# Another comment\nport = 8080\n";
+  std::string content =
+      "[http]\n# This is a comment\nserver_name = webserv\n# Another "
+      "comment\nport = 8080\n";
   Directive* directive = parser->parseFromString(content);
-  
+
   ASSERT_NE(directive, (Directive*)NULL);
   EXPECT_EQ(directive->children().size(), 1);
   EXPECT_EQ(directive->children()[0].name(), "http");
   EXPECT_EQ(directive->children()[0].keyValues().size(), 2);
-  EXPECT_EQ(directive->children()[0].keyValues().at("server_name")[0], "webserv");
+  EXPECT_EQ(directive->children()[0].keyValues().at("server_name")[0],
+            "webserv");
   EXPECT_EQ(directive->children()[0].keyValues().at("port")[0], "8080");
-  
+
   delete directive;
 }
 
 // ネストされたセクションのテスト
 TEST_F(TOMLParserTest, NestedSections) {
-  std::string content = "[http]\n[http.server]\nname = \"main_server\"\n[http.server.location]\npath = \"/\"\n";
+  std::string content =
+      "[http]\n[http.server]\nname = "
+      "\"main_server\"\n[http.server.location]\npath = \"/\"\n";
   Directive* directive = parser->parseFromString(content);
-  
+
   ASSERT_NE(directive, (Directive*)NULL);
   EXPECT_EQ(directive->children().size(), 1);
   EXPECT_EQ(directive->children()[0].name(), "http");
   EXPECT_EQ(directive->children()[0].children().size(), 1);
   EXPECT_EQ(directive->children()[0].children()[0].name(), "server");
-  EXPECT_EQ(directive->children()[0].children()[0].keyValues().at("name")[0], "main_server");
+  EXPECT_EQ(directive->children()[0].children()[0].keyValues().at("name")[0],
+            "main_server");
   EXPECT_EQ(directive->children()[0].children()[0].children().size(), 1);
-  EXPECT_EQ(directive->children()[0].children()[0].children()[0].name(), "location");
-  EXPECT_EQ(directive->children()[0].children()[0].children()[0].keyValues().at("path")[0], "/");
-  
+  EXPECT_EQ(directive->children()[0].children()[0].children()[0].name(),
+            "location");
+  EXPECT_EQ(directive->children()[0].children()[0].children()[0].keyValues().at(
+                "path")[0],
+            "/");
+
   delete directive;
 }
 
 // 引用符付き文字列のテスト
 TEST_F(TOMLParserTest, QuotedStrings) {
-  std::string content = "[http]\nserver_name = \"my webserv\"\nroot = '/var/www'\n";
+  std::string content =
+      "[http]\nserver_name = \"my webserv\"\nroot = '/var/www'\n";
   Directive* directive = parser->parseFromString(content);
-  
+
   ASSERT_NE(directive, (Directive*)NULL);
   EXPECT_EQ(directive->children().size(), 1);
-  EXPECT_EQ(directive->children()[0].keyValues().at("server_name")[0], "my webserv");
+  EXPECT_EQ(directive->children()[0].keyValues().at("server_name")[0],
+            "my webserv");
   EXPECT_EQ(directive->children()[0].keyValues().at("root")[0], "/var/www");
-  
+
   delete directive;
 }
 
 // エスケープシーケンスを含む文字列のテスト
 TEST_F(TOMLParserTest, EscapeSequences) {
-  std::string content = "[http]\nmessage = \"Hello\\nWorld\"\npath = \"C:\\\\Program Files\"\n";
+  std::string content =
+      "[http]\nmessage = \"Hello\\nWorld\"\npath = \"C:\\\\Program Files\"\n";
   Directive* directive = parser->parseFromString(content);
-  
+
   ASSERT_NE(directive, (Directive*)NULL);
   EXPECT_EQ(directive->children().size(), 1);
-  EXPECT_EQ(directive->children()[0].keyValues().at("message")[0], "Hello\nWorld");
-  EXPECT_EQ(directive->children()[0].keyValues().at("path")[0], "C:\\Program Files");
-  
+  EXPECT_EQ(directive->children()[0].keyValues().at("message")[0],
+            "Hello\nWorld");
+  EXPECT_EQ(directive->children()[0].keyValues().at("path")[0],
+            "C:\\Program Files");
+
   delete directive;
 }
 
 // 配列のテスト
 TEST_F(TOMLParserTest, Arrays) {
-  std::string content = "[http]\nallowed_methods = [\"GET\", \"POST\", \"DELETE\"]\nports = [8080, 8081]\n";
+  std::string content =
+      "[http]\nallowed_methods = [\"GET\", \"POST\", \"DELETE\"]\nports = "
+      "[8080, 8081]\n";
   Directive* directive = parser->parseFromString(content);
-  
+
   ASSERT_NE(directive, (Directive*)NULL);
   EXPECT_EQ(directive->children().size(), 1);
-  EXPECT_EQ(directive->children()[0].keyValues().at("allowed_methods").size(), 3);
-  EXPECT_EQ(directive->children()[0].keyValues().at("allowed_methods")[0], "GET");
-  EXPECT_EQ(directive->children()[0].keyValues().at("allowed_methods")[1], "POST");
-  EXPECT_EQ(directive->children()[0].keyValues().at("allowed_methods")[2], "DELETE");
+  EXPECT_EQ(directive->children()[0].keyValues().at("allowed_methods").size(),
+            3);
+  EXPECT_EQ(directive->children()[0].keyValues().at("allowed_methods")[0],
+            "GET");
+  EXPECT_EQ(directive->children()[0].keyValues().at("allowed_methods")[1],
+            "POST");
+  EXPECT_EQ(directive->children()[0].keyValues().at("allowed_methods")[2],
+            "DELETE");
   EXPECT_EQ(directive->children()[0].keyValues().at("ports").size(), 2);
   EXPECT_EQ(directive->children()[0].keyValues().at("ports")[0], "8080");
   EXPECT_EQ(directive->children()[0].keyValues().at("ports")[1], "8081");
-  
+
   delete directive;
 }
 
@@ -165,11 +183,12 @@ TEST_F(TOMLParserTest, Arrays) {
 // TEST_F(TOMLParserTest, EmptyArray) {
 //   std::string content = "[http]\nempty_array = []\n";
 //   Directive* directive = parser->parseFromString(content);
-  
+
 //   ASSERT_NE(directive, (Directive*)NULL);
 //   EXPECT_EQ(directive->children().size(), 1);
-//   EXPECT_EQ(directive->children()[0].keyValues().at("empty_array").size(), 0);
-  
+//   EXPECT_EQ(directive->children()[0].keyValues().at("empty_array").size(),
+//   0);
+
 //   delete directive;
 // }
 
@@ -207,7 +226,8 @@ TEST_F(TOMLParserTest, InvalidSectionHeader) {
 
 // 重複キーのテスト
 TEST_F(TOMLParserTest, DuplicateKey) {
-  std::string content = "[http]\nserver_name = webserv\nserver_name = another\n";
+  std::string content =
+      "[http]\nserver_name = webserv\nserver_name = another\n";
   Directive* directive = parser->parseFromString(content);
   EXPECT_EQ(directive, (Directive*)NULL);
 }
@@ -228,14 +248,16 @@ TEST_F(TOMLParserTest, TableArrayRejection) {
 
 // インラインテーブルの拒否テスト
 TEST_F(TOMLParserTest, InlineTableRejection) {
-  std::string content = "[http]\nserver = { name = \"webserv\", port = 8080 }\n";
+  std::string content =
+      "[http]\nserver = { name = \"webserv\", port = 8080 }\n";
   Directive* directive = parser->parseFromString(content);
   EXPECT_EQ(directive, (Directive*)NULL);
 }
 
 // セクションとキーの衝突テスト
 TEST_F(TOMLParserTest, SectionKeyConflict) {
-  std::string content = "[http]\nserver = value\n[http.server]\nname = \"main_server\"\n";
+  std::string content =
+      "[http]\nserver = value\n[http.server]\nname = \"main_server\"\n";
   Directive* directive = parser->parseFromString(content);
   EXPECT_EQ(directive, (Directive*)NULL);
 }
@@ -279,11 +301,12 @@ TEST_F(TOMLParserTest, InvalidEscapeSequence) {
 TEST_F(TOMLParserTest, QuotedKeys) {
   std::string content = "[http]\n\"server_name\" = webserv\n'port' = 8080\n";
   Directive* directive = parser->parseFromString(content);
-  
+
   ASSERT_NE(directive, (Directive*)NULL);
   EXPECT_EQ(directive->children().size(), 1);
-  EXPECT_EQ(directive->children()[0].keyValues().at("\"server_name\"")[0], "webserv");
+  EXPECT_EQ(directive->children()[0].keyValues().at("\"server_name\"")[0],
+            "webserv");
   EXPECT_EQ(directive->children()[0].keyValues().at("'port'")[0], "8080");
-  
+
   delete directive;
 }
