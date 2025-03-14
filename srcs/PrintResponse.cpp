36328b6643486941fcc9ab4df4f207d064bac9ce
JIA0010 <HIROsroutine@gmail.com>
2025-03-14 18:41:23 +0900

    仮のクラス。HandleError.hpp HandleSuccess.hppが必要

---
+++ srcs/PrintResponse.cpp
@@ -0,0 +1,11 @@
+#include "PrintResponse.hpp"
+
+PrintResponse::PrintResponse() {}
+
+PrintResponse::~PrintResponse() {}
+
+void PrintResponse::print_response() 
+{
+    //PrintResponseの中身の
+    send(fd, string , strlen , カスタムフラグ);
+}
---
+++ srcs/PrintResponse.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "HandleSuccess.hpp"
+#include "HandleError.hpp"
+
+class PrintResponse {
+ public:
+  PrintResponse();
+  ~PrintResponse();
+  void print_response();
+};
