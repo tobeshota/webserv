158b86d741b67313bbd337854cc7de3aadbbd3ac
tobeshota <Shota.Tobe@keio.jp>
2025-03-17 15:53:56 +0900

    ref: run make fmt

--- srcs/HandleError.cpp
+++ srcs/HandleError.cpp
@@ -1,17 +1,19 @@
 #include "HandleError.hpp"
 
 std::string int2str(int nb) {
-    std::stringstream ss;
-    ss << nb;
-    return ss.str();
+  std::stringstream ss;
+  ss << nb;
+  return ss.str();
 }
 
 std::string HandleError::generateHttpStatusLine(const int status_code) {
   StatusCodes statusCodes;
 
-  std::string httpStatusLine = std::string(_httpRequest.getVersion()) + " ";  //  http version
-  httpStatusLine += int2str(status_code);  //  http status code
-  httpStatusLine += " " + statusCodes.getMessage(status_code) + "\n";  //  http status message
+  std::string httpStatusLine =
+      std::string(_httpRequest.getVersion()) + " ";  //  http version
+  httpStatusLine += int2str(status_code);            //  http status code
+  httpStatusLine +=
+      " " + statusCodes.getMessage(status_code) + "\n";  //  http status message
   return httpStatusLine;
 }
 
@@ -33,8 +35,9 @@ std::string readFile(const std::string& filePath) {
 }
 
 // DateのHTTPレスポンスヘッダは省略する．必要になったら設定する．
-// #include <ctime>  // 時刻の取得や操作を行うためのヘッダ。time(), gmtime(), struct tmなどの機能を提供。
-// #include <iomanip>  // 入力/出力のフォーマット設定を行うためのヘッダ。std::setfill('0')やstd::setw(2)など、表示フォーマットを制御するために使用。
+// #include <ctime>  // 時刻の取得や操作を行うためのヘッダ。time(), gmtime(),
+// struct tmなどの機能を提供。 #include <iomanip>  //
+// 入力/出力のフォーマット設定を行うためのヘッダ。std::setfill('0')やstd::setw(2)など、表示フォーマットを制御するために使用。
 // std::string getCurrentTimeInGMTFormat() {
 //   // 現在の時刻を取得
 //   time_t rawtime;
@@ -53,7 +56,8 @@ std::string readFile(const std::string& filePath) {
 
 //   // 現在時刻を指定の形式でストリームに追加
 //   result << weekdays[timeinfo->tm_wday] << ", " << timeinfo->tm_mday << " "
-//          << months[timeinfo->tm_mon] << " " << (1900 + timeinfo->tm_year) << " "
+//          << months[timeinfo->tm_mon] << " " << (1900 + timeinfo->tm_year) <<
+//          " "
 //          << std::setfill('0') << std::setw(2) << timeinfo->tm_hour << ":"
 //          << std::setfill('0') << std::setw(2) << timeinfo->tm_min << ":"
 //          << std::setfill('0') << std::setw(2) << timeinfo->tm_sec << " GMT";
@@ -67,7 +71,8 @@ std::string HandleError::generateHttpResponseHeader(
   std::string httpResponseHeader = "Server: webserv\n";
   // httpResponseHeader += "Date: " + getCurrentTimeInGMTFormat() + "\n";
   httpResponseHeader += "Content-Type: text/html\n";
-  httpResponseHeader += "Content-Length: " + int2str(httpResponseBody.size()) + "\n";
+  httpResponseHeader +=
+      "Content-Length: " + int2str(httpResponseBody.size()) + "\n";
   httpResponseHeader += "Connection: close\n";
   return httpResponseHeader;
 }
@@ -77,12 +82,13 @@ std::string HandleError::generateHttpResponseHeader(
 std::string HandleError::generateHttpResponseBody(const int status_code) {
   std::string httpResponseBody, errorPageValue, rootValue;
 
-  const Directive *errorPageDirective = _rootDirective.findDirective(_httpRequest.getHeader("Host"), "error_page");
-  if (errorPageDirective == NULL)
-    return readFile(DEFAULT_ERROR_PAGE);
+  const Directive* errorPageDirective = _rootDirective.findDirective(
+      _httpRequest.getHeader("Host"), "error_page");
+  if (errorPageDirective == NULL) return readFile(DEFAULT_ERROR_PAGE);
   errorPageValue = errorPageDirective->getValue(int2str(status_code));
 
-  const Directive* hostDirective = _rootDirective.findDirective(_httpRequest.getHeader("Host"));
+  const Directive* hostDirective =
+      _rootDirective.findDirective(_httpRequest.getHeader("Host"));
   if (hostDirective != NULL) {
     rootValue = hostDirective->getValue("root");
   }
@@ -98,14 +104,15 @@ std::string HandleError::generateHttpResponseBody(const int status_code) {
 }
 
 HandleError::HandleError(Directive rootDirective, HTTPRequest httpRequest)
-    : _rootDirective(rootDirective),
-    _httpRequest(httpRequest) {}
+    : _rootDirective(rootDirective), _httpRequest(httpRequest) {}
 
 void HandleError::handleRequest(HTTPResponse& httpResponse) {
-  httpResponse.setHttpStatusLine( this->generateHttpStatusLine(httpResponse.getHttpStatusCode()));
-  httpResponse.setHttpResponseBody(this->generateHttpResponseBody(httpResponse.getHttpStatusCode()));
-  httpResponse.setHttpResponseHeader( this->generateHttpResponseHeader(httpResponse.getHttpResponseBody()));
+  httpResponse.setHttpStatusLine(
+      this->generateHttpStatusLine(httpResponse.getHttpStatusCode()));
+  httpResponse.setHttpResponseBody(
+      this->generateHttpResponseBody(httpResponse.getHttpStatusCode()));
+  httpResponse.setHttpResponseHeader(
+      this->generateHttpResponseHeader(httpResponse.getHttpResponseBody()));
 
-  if (_nextHandler != NULL)
-    _nextHandler->handleRequest(httpResponse);
+  if (_nextHandler != NULL) _nextHandler->handleRequest(httpResponse);
 }
