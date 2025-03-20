bdba5f6cc93b1b229899b96fae0504c3792a7337
tobeshota <Shota.Tobe@keio.jp>
2025-03-20 12:52:26 +0900

    feat: enable to get `_port` and `_server_name`

--- srcs/HTTPRequest.cpp
+++ srcs/HTTPRequest.cpp
@@ -12,9 +12,23 @@ HTTPRequest::HTTPRequest(const std::string& method, const std::string& url,
       _headers(headers),
       _body(body),
       _valid(true),
-      _keepAlive(keepAlive) {}
-
-HTTPRequest::~HTTPRequest() {}
+      _keepAlive(keepAlive) {
+  // Hostヘッダがある場合は、_server_nameと_portに値を設定
+  std::map<std::string, std::string>::const_iterator it = _headers.find("Host");
+  if (it != _headers.end()) {
+    std::string host = it->second;
+    size_t pos = host.find(":");
+    if (pos != std::string::npos) {
+      _server_name = host.substr(0, pos);
+      _port = host.substr(pos + 1);
+    } else {
+      // 絶対に来ない
+      _server_name = "localhost";
+      _port = "8080";
+    }
+  }
+}
+HTTPRequest::~HTTPRequest() { ; }
 
 std::string HTTPRequest::getHeader(const std::string& key) const {
   std::map<std::string, std::string>::const_iterator it = _headers.find(key);
--- srcs/HTTPRequest.hpp
+++ srcs/HTTPRequest.hpp
@@ -10,6 +10,8 @@ class HTTPRequest {
   std::string _version;
   std::map<std::string, std::string> _headers;
   std::string _body;
+  std::string _server_name;
+  std::string _port;
   bool _valid;
   bool _keepAlive;  // Keep-Alive接続かどうかを示すフラグを追加
 
@@ -28,6 +30,8 @@ class HTTPRequest {
   std::string getURL() const { return _url; }
   std::string getVersion() const { return _version; }
   std::string getBody() const { return _body; }
+  std::string getServerName() const { return _server_name; }
+  std::string getPort() const { return _port; }
   const std::map<std::string, std::string>& getHeaders() const {
     return _headers;
   }
