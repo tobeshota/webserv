a0b83e3d551fa2135a0eb3a93bec4f3cd0157b9b
tobeshota <Shota.Tobe@keio.jp>
2025-03-23 14:05:10 +0900

    ref: run `make fmt`

--- srcs/GenerateHTTPResponse.cpp
+++ srcs/GenerateHTTPResponse.cpp
@@ -176,7 +176,8 @@ std::string GenerateHTTPResponse::generateHttpResponseBody(
   }
   // ディレクトリリスニングすべきか
   else if (getDirectiveValue("autoindex") == "on" &&
-           getDirectiveValue("root") != "" && isDirectory(getDirectiveValue("root") + _httpRequest.getURL())) {
+           getDirectiveValue("root") != "" &&
+           isDirectory(getDirectiveValue("root") + _httpRequest.getURL())) {
     ListenDirectory listenDirectory(getDirectiveValue("root") +
                                     _httpRequest.getURL());
     HTTPResponse response;
